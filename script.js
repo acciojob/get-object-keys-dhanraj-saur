a7ed1f56a0c8a09cdbbf4c67622c8811d424edb7
dhanraj-saur <121377364+dhanraj-saur@users.noreply.github.com>
2023-04-14 00:18:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+var student = {
+                name : "David Rayy", 
+                sclass : "VI", 
+                rollno : 12 
+               } 
+var keys = Object.keys(student);
+document.getElementById("obj").innerHTML = keys;
